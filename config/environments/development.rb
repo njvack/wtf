944d159659975d42054f65153e7f4563d5b313ca
Nate Vack <njvack@wisc.edu>
2017-05-05 15:15:29 -0500

    Remove dependency on listen gem
    all the way, now

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,5 +50,6 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # Actually don't use this, it breaks OS X.
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
